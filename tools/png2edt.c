4329fe1414a65baa69d2b73b5900b7d313510829
slembcke <slembcke@gmail.com>
2019-03-14 00:20:33 -0500

    png2edt mostly working?

--- tools/png2edt.c
+++ tools/png2edt.c
@@ -3,14 +3,13 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <assert.h>
 
 #include "shared.h"
 
-#define MAX 256
+#define MAX 128
 
 typedef struct {uint x, y;} uvec;
-
-static void dbg(){}
 
 static float intersect_parabolas(uvec a, uvec b){
 	int num = (a.x*a.x + a.y) - (b.x*b.x + b.y);
@@ -22,13 +21,13 @@ static float intersect_parabolas(uvec a, uvec b){
 uvec v[1024];
 float z[1024];
 
-static void build_hull(uint dist[], uint count){
-	v[0] = (uvec){0, dist[0]};
+static void build_hull(uint edt[], uint count){
+	v[0] = (uvec){0, edt[0]};
 	z[0] = -MAX, z[1] = MAX;
 	
 	uint k = 0;
 	for(uint i = 1; i < count; i++){
-		uvec p = v[k], q = {i, dist[i]};
+		uvec p = v[k], q = {i, edt[i]};
 		float s = intersect_parabolas(p, q);
 		
 		while(s <= z[k]){
@@ -42,37 +41,34 @@ static void build_hull(uint dist[], uint count){
 	}
 }
 
-static void march_parabolas(uint dist[], uint count, uint out[], uint stride){
+static void march_parabolas(uint count, uint output[], uint stride){
 	uint j = 0;
 	for(uint i = 0; i < count; i++){
 		while(z[j + 1] < i) j += 1;
 		int dx = i - v[j].x;
-		out[i*stride] = dx*dx + v[j].y;
+		output[i*stride] = dx*dx + v[j].y;
 	}
 }
 
-static void scan_pass(uint *output, uint *input, uint w, uint h){
+static void scan_pass(uint output[], uint input[], uint w, uint h){
 	for(uint row = 0; row < h; row++){
-		uint *image_row = input + row*w;
-		build_hull(image_row, w);
-		march_parabolas(image_row, w, output + row, w);
+		build_hull(input + row*w, w);
+		march_parabolas(w, output + row, h);
 	}
 }
 
-static uint *generate_edt(uint w, uint h){
-	uint edt_size = sizeof(uint)*w*h;
-	uint *buff0 = malloc(2*edt_size);
-	uint *buff1 = buff0 + edt_size;
+static uint *generate_edt(Image image){
+	uint edt_len = image.w*image.h;
+	uint *buff0 = malloc(2*sizeof(*buff0)*edt_len);
+	uint *buff1 = buff0 + edt_len;
+	buff1[0] = 0;
 	
-	for(uint y = 0; y < h; y++){
-		for(uint x = 0; x < w; x++) buff0[x + y*w] = MAX;
-	}
-	buff0[0] = 0;
-	buff0[255] = 0;
+	// Convert to initial EDT values.
+	for(uint i = 0; i < edt_len; i++) buff0[i] = (image.pixels[i] ? MAX : 0);
 	
-	scan_pass(buff1, buff0, w, h);
-	scan_pass(buff0, buff1, h, w);
-	return realloc(buff0, edt_size);
+	scan_pass(buff1, buff0, image.w, image.h);
+	scan_pass(buff0, buff1, image.h, image.w);
+	return realloc(buff0, sizeof(*buff0)*edt_len);
 }
 
 int main(int argc, char *argv[]){
@@ -84,21 +80,11 @@ int main(int argc, char *argv[]){
 	// FILE *outfile = fopen(argv[2], "w");
 	// SLIB_ASSERT_HARD(outfile, "Can't open output file '%s'", argv[2]);
 	
-	// Image image = read_png(infile);
-	// for(uint r = 0; r < image.h/8; r++) {
-	// 	for(uint c = 0; c < image.w/8; c++) {
-	// 		const u8 *pixels = image.pixels + 8*(c + r*image.w);
-	// 		u8 tile[16] = {};
-	// 		tile_to_chr(pixels, image.w, tile);
-	// 		fwrite(&tile, sizeof(tile), 1, outfile);
-	// 	}
-	// }
-	
-	Image image = {.w = 16, .h = 16};
-	uint *edt = generate_edt(image.w, image.h);
+	Image image = read_png(infile);
+	uint *edt = generate_edt(image);
 	
 	for(uint y = 0; y < image.h; y++){
-		for(uint x = 0; x < image.w; x++) printf("% 4d, ", edt[x + y*image.w]);
+		for(uint x = 0; x < image.w; x++) printf("%02X ", edt[x + y*image.w]);
 		printf("\n");
 	}
 	
